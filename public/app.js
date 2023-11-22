65af84079c75f1c9b1041e02dfdbf0140eb54958
Allison <farrella95@gmail.com>
2023-11-22 13:56:38 -0500

    basic calculator function done

--- public/app.js
+++ public/app.js
@@ -1,13 +1,33 @@
-let billInput = document.querySelector(".bill-input").value;
-let tipAmountBtn = document.querySelector(".tip-amount-select");
-let customAmount = document.querySelector("#custom").value;
-let peopleInput = document.querySelector(".people-input").value;
+let billInput = document.querySelector(".bill-input");
+let tipAmountBtn = document.querySelectorAll(".tip-amount-select");
+let customAmount = document.querySelector("#custom");
+let peopleInput = document.querySelector(".people-input");
 let tipPerPersonTotal = document.querySelector(".tip-total-pp");
 let totalPerPerson = document.querySelector(".total-pp");
 let reset = document.querySelector(".reset");
 
-console.log(billInput);
+tipPerPersonTotal.innerHTML = "$0.00";
+totalPerPerson.innerHTML = "$0.00";
 
+function calculate(event) {
+  let bill = billInput.value;
+  let people = peopleInput.value;
+
+  const dataValue = parseFloat(event.target.getAttribute("data-value"));
+
+  let totalTip = (bill * dataValue) / people;
+  let totalBill = bill / people + totalTip / people;
+  console.log(totalTip);
+  console.log(totalBill);
+
+  tipPerPersonTotal.innerHTML = `$${totalTip.toFixed(2)}`;
+  totalPerPerson.innerHTML = `$${totalBill.toFixed(2)}`;
+}
+
+// Add click event listeners to tip amount buttons
+tipAmountBtn.forEach((button) => {
+  button.addEventListener("click", calculate);
+});
 //reset calculator
 reset.addEventListener("click", () => {
   window.location.reload();
