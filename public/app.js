86b76fd5c504916acceda2620f861eb17ef803f9
Allison <farrella95@gmail.com>
2023-11-22 09:40:06 -0500

    javascript logic start

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-//add logic
+let billInput = document.querySelector(".bill-input").value;
+let tipAmountBtn = document.querySelector(".tip-amount-select");
+let customAmount = document.querySelector("#custom").value;
+let peopleInput = document.querySelector(".people-input").value;
+let tipPerPersonTotal = document.querySelector(".tip-total-pp");
+let totalPerPerson = document.querySelector(".total-pp");
+let reset = document.querySelector(".reset");
+
+console.log(billInput);
+
+//reset calculator
+reset.addEventListener("click", () => {
+  window.location.reload();
+});
